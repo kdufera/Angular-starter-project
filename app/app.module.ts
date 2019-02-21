a20164a2a32734773c935cb05d5a99f2d9f108a8
Kaleb Dufera <kdufera@gmail.com>
2019-02-21 18:51:41 -0500

    nav bar update

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,12 +7,18 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
